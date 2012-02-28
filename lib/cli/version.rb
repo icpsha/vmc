f8be1763cd3d9801c057738c80bc374370585641
i033053 <shahul.hameed.p@sap.com>
2012-02-27 16:14:31 -0800

    updated the gemspec for virgo

--- lib/cli/version.rb
+++ lib/cli/version.rb
@@ -3,5 +3,6 @@ module VMC
     # This version number is used as the RubyGem release version.
     # The internal VMC version number is VMC::VERSION.
     VERSION = '0.3.16.beta.3'
+    VIR_VERSION = '0.0.1.beta'
   end
 end
